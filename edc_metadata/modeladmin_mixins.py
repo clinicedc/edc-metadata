66cce6d3429884c80d300d09c5643879ce339a7d
erikvw <ew2789@gmail.com>
2018-01-09 09:49:46 +0200

    show visit code sequence in admin

--- edc_metadata/modeladmin_mixins.py
+++ edc_metadata/modeladmin_mixins.py
@@ -1,31 +1,38 @@
-from django.contrib import admin
+
+class Base:
+
+    def seq(self, obj=None):
+        return obj.visit_code_sequence
 
 
-class CrfMetaDataAdminMixin(admin.ModelAdmin):
+class CrfMetaDataAdminMixin(Base):
 
     search_fields = (
         'subject_identifier', 'model', 'id')
     list_display = (
-        'subject_identifier', 'model', 'visit_code', 'entry_status', 'fill_datetime',
+        'subject_identifier', 'model', 'visit_code', 'seq', 'entry_status', 'fill_datetime',
         'due_datetime', 'close_datetime', 'created', 'hostname_created')
     list_filter = (
-        'entry_status', 'visit_code', 'schedule_name', 'visit_schedule_name',
-        'model', 'fill_datetime', 'created', 'user_created', 'hostname_created')
+        'entry_status', 'visit_code', 'visit_code_sequence', 'schedule_name',
+        'visit_schedule_name', 'model', 'fill_datetime', 'created',
+        'user_created', 'hostname_created')
     readonly_fields = ('subject_identifier', 'model', 'visit_code',
                        'schedule_name', 'visit_schedule_name',
                        'show_order', 'current_entry_title')
 
 
-class RequisitionMetaDataAdminMixin:
+class RequisitionMetaDataAdminMixin(Base):
 
     search_fields = (
         'subject_identifier', 'model', 'id', 'panel')
     list_display = (
-        'subject_identifier', 'model', 'panel', 'visit_code', 'entry_status', 'fill_datetime',
-        'due_datetime', 'close_datetime', 'created', 'hostname_created')
+        'subject_identifier', 'model', 'panel', 'visit_code', 'seq',
+        'entry_status', 'fill_datetime', 'due_datetime', 'close_datetime',
+        'created', 'hostname_created')
     list_filter = (
-        'entry_status', 'panel', 'visit_code', 'schedule_name', 'visit_schedule_name',
-        'model', 'fill_datetime', 'created', 'user_created', 'hostname_created')
+        'entry_status', 'panel', 'visit_code', 'visit_code_sequence',
+        'schedule_name', 'visit_schedule_name', 'model', 'fill_datetime',
+        'created', 'user_created', 'hostname_created')
     readonly_fields = ('subject_identifier', 'model', 'visit_code',
                        'schedule_name', 'visit_schedule_name',
                        'show_order', 'current_entry_title')
