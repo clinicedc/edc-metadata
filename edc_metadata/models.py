466c73328115f4d43f1e0119f9cc527b4bb9cb56
erikvw <ew2789@gmail.com>
2018-01-16 19:39:56 +0200

    add support for sites

--- edc_metadata/models.py
+++ edc_metadata/models.py
@@ -1,20 +1,34 @@
 from edc_base.model_mixins import BaseUuidModel
+from edc_base.sites.managers import CurrentSiteManager
+from edc_base.sites.site_model_mixin import SiteModelMixin
 
 from .managers import CrfMetadataManager, RequisitionMetadataManager
 from .model_mixins.metadata_models import CrfModelMixin, RequisitionMetadataModelMixin
 
 
-class CrfMetadata(CrfModelMixin, BaseUuidModel):
+class CrfMetadata(CrfModelMixin, SiteModelMixin, BaseUuidModel):
+
+    on_site = CurrentSiteManager()
 
     objects = CrfMetadataManager()
+
+    def natural_key(self):
+        return super().natural_key()
+    natural_key.dependencies = ['sites.Site']
 
     class Meta(CrfModelMixin.Meta):
         app_label = 'edc_metadata'
 
 
-class RequisitionMetadata(RequisitionMetadataModelMixin, BaseUuidModel):
+class RequisitionMetadata(RequisitionMetadataModelMixin, SiteModelMixin, BaseUuidModel):
+
+    on_site = CurrentSiteManager()
 
     objects = RequisitionMetadataManager()
+
+    def natural_key(self):
+        return super().natural_key()
+    natural_key.dependencies = ['sites.Site']
 
     class Meta(RequisitionMetadataModelMixin.Meta):
         app_label = 'edc_metadata'
