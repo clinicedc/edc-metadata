914e372e37f3dc1697f2aaee6510bc070b05320f
erikvw <ew2789@gmail.com>
2018-06-25 18:15:48 +0200

    move dummy panel to edc_visit_schedule

--- edc_metadata/tests/visit_schedule.py
+++ edc_metadata/tests/visit_schedule.py
@@ -2,26 +2,10 @@ from dateutil.relativedelta import relativedelta
 
 from edc_visit_schedule import VisitSchedule, Schedule, Visit
 from edc_visit_schedule import FormsCollection, Crf, Requisition
+from edc_visit_schedule.tests import DummyPanel
 
 
 app_label = 'edc_metadata'
-
-
-class DummyPanel:
-
-    """A dummy lab panel object.
-    """
-
-    def __init__(self, name=None, verbose_name=None, requisition_model=None):
-        self.name = name
-        self.verbose_name = verbose_name or name
-        self.requisition_model = requisition_model
-
-    def __str__(self):
-        return self.verbose_name
-
-    def __repr__(self):
-        return f'{self.__class__.__name__}(name=\'{self.name}\')'
 
 
 class MockPanel(DummyPanel):
