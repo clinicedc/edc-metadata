5b9d647efa855f292788d49c95ab888ec454e7ee
erikvw <ew2789@gmail.com>
2021-04-22 19:27:01 -0500

    fix import

--- edc_metadata/tests/tests/test_view_mixin.py
+++ edc_metadata/tests/tests/test_view_mixin.py
@@ -15,8 +15,6 @@ from edc_utils import get_utcnow
 from edc_visit_schedule.site_visit_schedules import site_visit_schedules
 from edc_visit_tracking.constants import SCHEDULED
 
-from edc_metadata.tests.tests.metadata_test_mixin import TestMetadataMixin
-
 from ...models import CrfMetadata, RequisitionMetadata
 from ...view_mixins import MetaDataViewMixin
 from ..models import CrfOne, CrfThree, SubjectConsent, SubjectVisit
