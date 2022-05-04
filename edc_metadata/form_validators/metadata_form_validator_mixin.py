cad189660e0ddfbfbb240daa7d7cdfb7d5ca778e
erikvw <ew2789@gmail.com>
2022-05-03 23:16:28 -0500

    utils to access models

--- edc_metadata/form_validators/metadata_form_validator_mixin.py
+++ edc_metadata/form_validators/metadata_form_validator_mixin.py
@@ -1,5 +1,5 @@
 from ..constants import REQUIRED
-from ..models import CrfMetadata, RequisitionMetadata
+from ..utils import get_crf_metadata_model_cls, get_requisition_metadata_model_cls
 
 
 class MetaDataFormValidatorMixin:
@@ -9,49 +9,65 @@ class MetaDataFormValidatorMixin:
     @property
     def crf_metadata_exists(self) -> bool:
         """Returns True if CRF metadata exists for this visit code."""
-        return CrfMetadata.objects.filter(
-            subject_identifier=self.instance.subject_identifier,
-            visit_schedule_name=self.instance.visit_schedule_name,
-            schedule_name=self.instance.schedule_name,
-            visit_code=self.instance.visit_code,
-            visit_code_sequence=self.instance.visit_code_sequence,
-        ).exists()
+        return (
+            get_crf_metadata_model_cls()
+            .objects.filter(
+                subject_identifier=self.instance.subject_identifier,
+                visit_schedule_name=self.instance.visit_schedule_name,
+                schedule_name=self.instance.schedule_name,
+                visit_code=self.instance.visit_code,
+                visit_code_sequence=self.instance.visit_code_sequence,
+            )
+            .exists()
+        )
 
     @property
     def crf_metadata_required_exists(self) -> bool:
         """Returns True if any required CRFs for this visit code have
         not yet been keyed.
         """
-        return CrfMetadata.objects.filter(
-            subject_identifier=self.instance.subject_identifier,
-            visit_schedule_name=self.instance.visit_schedule_name,
-            schedule_name=self.instance.schedule_name,
-            visit_code=self.instance.visit_code,
-            visit_code_sequence=self.instance.visit_code_sequence,
-            entry_status=REQUIRED,
-        ).exists()
+        return (
+            get_crf_metadata_model_cls()
+            .objects.filter(
+                subject_identifier=self.instance.subject_identifier,
+                visit_schedule_name=self.instance.visit_schedule_name,
+                schedule_name=self.instance.schedule_name,
+                visit_code=self.instance.visit_code,
+                visit_code_sequence=self.instance.visit_code_sequence,
+                entry_status=REQUIRED,
+            )
+            .exists()
+        )
 
     @property
     def requisition_metadata_exists(self) -> bool:
         """Returns True if requisition metadata exists for this visit code."""
-        return RequisitionMetadata.objects.filter(
-            subject_identifier=self.instance.subject_identifier,
-            visit_schedule_name=self.instance.visit_schedule_name,
-            schedule_name=self.instance.schedule_name,
-            visit_code=self.instance.visit_code,
-            visit_code_sequence=self.instance.visit_code_sequence,
-        ).exists()
+        return (
+            get_requisition_metadata_model_cls()
+            .objects.filter(
+                subject_identifier=self.instance.subject_identifier,
+                visit_schedule_name=self.instance.visit_schedule_name,
+                schedule_name=self.instance.schedule_name,
+                visit_code=self.instance.visit_code,
+                visit_code_sequence=self.instance.visit_code_sequence,
+            )
+            .exists()
+        )
 
     @property
     def requisition_metadata_required_exists(self) -> bool:
         """Returns True if any required requisitions for this visit code
         have not yet been keyed.
         """
-        return RequisitionMetadata.objects.filter(
-            subject_identifier=self.instance.subject_identifier,
-            visit_schedule_name=self.instance.visit_schedule_name,
-            schedule_name=self.instance.schedule_name,
-            visit_code=self.instance.visit_code,
-            visit_code_sequence=self.instance.visit_code_sequence,
-            entry_status=REQUIRED,
-        ).exists()
+        return (
+            get_requisition_metadata_model_cls()
+            .objects.filter(
+                subject_identifier=self.instance.subject_identifier,
+                visit_schedule_name=self.instance.visit_schedule_name,
+                schedule_name=self.instance.schedule_name,
+                visit_code=self.instance.visit_code,
+                visit_code_sequence=self.instance.visit_code_sequence,
+                entry_status=REQUIRED,
+            )
+            .exists()
+        )
