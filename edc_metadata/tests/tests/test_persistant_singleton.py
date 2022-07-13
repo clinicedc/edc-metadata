a7a202afe818c472700d0def643a5f5aece024a1
erikvw <ew2789@gmail.com>
2022-07-13 09:59:53 -0500

    lint

--- edc_metadata/tests/tests/test_persistant_singleton.py
+++ edc_metadata/tests/tests/test_persistant_singleton.py
@@ -3,14 +3,20 @@ from copy import deepcopy
 
 from dateutil.relativedelta import relativedelta
 from django import forms
+from django.conf import settings
 from django.contrib.auth.models import User
-from django.test import TestCase, override_settings, tag
+from django.contrib.sites.models import Site
+from django.test import TestCase, override_settings
+from edc_appointment.constants import IN_PROGRESS_APPT, INCOMPLETE_APPT
 from edc_appointment.models import Appointment
+from edc_constants.constants import FEMALE
+from edc_facility import import_holidays
 from edc_reference import site_reference_configs
 from edc_utils import get_utcnow
 from edc_visit_schedule import site_visit_schedules
 from edc_visit_schedule.constants import DAY1, MONTH1, MONTH3, MONTH6, WEEK2
 from edc_visit_tracking.constants import SCHEDULED
+from model_bakery import baker
 
 from edc_metadata import (
     KEYED,
@@ -43,19 +49,21 @@ class CrfOneForm(forms.ModelForm):
     EDC_PROTOCOL_STUDY_CLOSE_DATETIME=get_utcnow() + relativedelta(years=3),
 )
 class TestPersistantSingleton(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        import_holidays()
+
     def setUp(self):
-        if self.visit_schedule:
-            site_visit_schedules._registry = {}
-            site_visit_schedules.loaded = False
-            site_visit_schedules.register(visit_schedule)
+        site_visit_schedules._registry = {}
+        site_visit_schedules.loaded = False
+        site_visit_schedules.register(visit_schedule)
 
         site_reference_configs.register_from_visit_schedule(
             visit_models={"edc_appointment.appointment": "edc_metadata.subjectvisit"}
         )
 
         site_metadata_rules.registry = OrderedDict()
-        for rule_cls in self.rule_groups:
-            site_metadata_rules.register(rule_cls)
+        site_metadata_rules.register(self.rule_group)
 
         self.user = User.objects.create(username="erik")
 
@@ -90,12 +98,76 @@ class TestPersistantSingleton(TestCase):
             f3="blah",
         )
 
-    def get_rule_group(self):
+    @staticmethod
+    def get_subject_consent():
+        return baker.make_recipe(
+            "edc_metadata.subjectconsent",
+            user_created="erikvw",
+            user_modified="erikvw",
+            screening_identifier="1234",
+            initials="XX",
+            gender=FEMALE,
+            dob=get_utcnow().date() - relativedelta(years=25),
+            site=Site.objects.get(id=settings.SITE_ID),
+            consent_datetime=get_utcnow() - relativedelta(months=24),
+        )
+
+    def get_subject_visit(
+        self,
+        visit_code=None,
+        visit_code_sequence=None,
+        reason=None,
+        appt_datetime=None,
+    ):
+        reason = reason or SCHEDULED
+        subject_consent = self.get_subject_consent()
+        options = dict(
+            subject_identifier=subject_consent.subject_identifier,
+            visit_code=visit_code or DAY1,
+            visit_code_sequence=(
+                visit_code_sequence if visit_code_sequence is not None else 0
+            ),
+            reason=reason,
+        )
+        if appt_datetime:
+            options.update(appt_datetime=appt_datetime)
+        appointment = self.get_appointment(**options)
+        subject_visit = SubjectVisit(
+            appointment=appointment,
+            reason=SCHEDULED,
+            report_datetime=appointment.appt_datetime,
+        )
+        subject_visit.save()
+        subject_visit.refresh_from_db()
+        return subject_visit
+
+    @staticmethod
+    def get_next_subject_visit(subject_visit):
+        appointment = subject_visit.appointment
+        appointment.appt_status = INCOMPLETE_APPT
+        appointment.save()
+        appointment.refresh_from_db()
+        next_appointment = appointment.next_by_timepoint
+        next_appointment.appt_status = IN_PROGRESS_APPT
+        next_appointment.save()
+        subject_visit = SubjectVisit(
+            appointment=next_appointment,
+            reason=SCHEDULED,
+            report_datetime=next_appointment.appt_datetime,
+            visit_code=next_appointment.visit_code,
+            visit_code_sequence=next_appointment.visit_code_sequence,
+        )
+        subject_visit.save()
+        subject_visit.refresh_from_db()
+        return subject_visit
+
+    @property
+    def rule_group(self):
         class Predicates(PersistantSingletonMixin, PredicateCollection):
             app_label = "edc_metadata"
             visit_model = "edc_metadata.subjectvisit"
 
-            def crfone_required(self, visit, **kwargs):
+            def crfone_required(self, visit, **kwargs):  # noqa
                 model = f"{self.app_label}.crfone"
                 return self.persistant_singleton_required(
                     visit, model=model, evaluate_after=[DAY1]
@@ -117,10 +189,9 @@ class TestPersistantSingleton(TestCase):
 
         return RuleGroup
 
-    @tag("1")
     def test_baseline_not_required(self):
         site_metadata_rules.registry = OrderedDict()
-        site_metadata_rules.register(self.get_rule_group())
+        site_metadata_rules.register(self.rule_group)
         form = CrfOneForm(data=self.data)
         form.is_valid()
         self.assertEqual({}, form._errors)
@@ -133,10 +204,9 @@ class TestPersistantSingleton(TestCase):
             ).exists()
         )
 
-    @tag("1")
     def test_1005_required(self):
         site_metadata_rules.registry = OrderedDict()
-        site_metadata_rules.register(self.get_rule_group())
+        site_metadata_rules.register(self.rule_group)
         subject_visit = self.get_next_subject_visit(self.subject_visit)
         self.assertEqual(subject_visit.visit_code, WEEK2)
         crf_metadata_getter = CrfMetadataGetter(appointment=subject_visit.appointment)
@@ -164,10 +234,9 @@ class TestPersistantSingleton(TestCase):
             ).exists()
         )
 
-    @tag("1")
     def test_visit_required_if_not_submitted(self):
         site_metadata_rules.registry = OrderedDict()
-        site_metadata_rules.register(self.get_rule_group())
+        site_metadata_rules.register(self.rule_group)
         subject_visit = self.get_next_subject_visit(self.subject_visit)
         self.assertEqual(subject_visit.visit_code, WEEK2)
         self.assertEqual(CrfMetadata.objects.filter(model="edc_metadata.crfone").count(), 1)
@@ -225,10 +294,9 @@ class TestPersistantSingleton(TestCase):
             ],
         )
 
-    @tag("1")
     def test_1010_required_if_not_submitted(self):
         site_metadata_rules.registry = OrderedDict()
-        site_metadata_rules.register(self.get_rule_group())
+        site_metadata_rules.register(self.rule_group)
         subject_visit = self.get_next_subject_visit(self.subject_visit)
         subject_visit = self.get_next_subject_visit(subject_visit)
         self.assertEqual(subject_visit.visit_code, MONTH1)
@@ -255,6 +323,8 @@ class TestPersistantSingleton(TestCase):
         )
 
     def test_1030_required_if_not_submitted(self):
+        site_metadata_rules.registry = OrderedDict()
+        site_metadata_rules.register(self.rule_group)
         subject_visit = self.get_next_subject_visit(self.subject_visit)
         subject_visit = self.get_next_subject_visit(subject_visit)
         subject_visit = self.get_next_subject_visit(subject_visit)
@@ -282,6 +352,8 @@ class TestPersistantSingleton(TestCase):
         )
 
     def test_1030_not_required_if_submitted(self):
+        site_metadata_rules.registry = OrderedDict()
+        site_metadata_rules.register(self.rule_group)
         subject_visit_1005 = self.get_next_subject_visit(self.subject_visit)
         subject_visit_1010 = self.get_next_subject_visit(subject_visit_1005)
         subject_visit_1030 = self.get_next_subject_visit(subject_visit_1010)
